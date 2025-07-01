caba1a0c55ca69be0425efe123619b7cdc146d6d
Ryan X. Charles <ryan@ryanxcharles.com>
2025-07-01 08:16:49 -0700

    oi

--- server.ts
+++ server.ts
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 import net from "node:net";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
 import express from "express";
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
