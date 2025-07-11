03900d20c57d439d1cb553f2c133c5561501df40
Ryan X. Charles <ryan@ryanxcharles.com>
2025-07-11 03:20:10 -0700

    v0.1.17

--- cli/cli-entry.ts
+++ cli/cli-entry.ts
@@ -59,7 +59,7 @@ program
   .name("termplot")
   .argument("[config]", "JSON plot configuration file (optional; can be piped)")
   .description("Termplot: Beautiful plots in your terminal")
-  .version("0.1.16")
+  .version("0.1.17")
   .action(async (input: string) => {
     let configStr = input;
     if (!configStr && !process.stdin.isTTY) {
